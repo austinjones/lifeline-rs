9a6a34c64c52d3c63d6281eaefbe14e4b909c527
Austin Jones <austinbaysjones@gmail.com>
2020-09-06 21:08:57 -0400

    Fix a build error if dyn-bus is unselected

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,8 @@
 //! Prelude, including all the traits and types required for typical lifeline usage.
 
 pub use crate::{
-    lifeline_bus, Bus, CarryFrom, CarryInto, Lifeline, Message, Receiver, Resource, Sender,
-    Service, Task,
+    Bus, CarryFrom, CarryInto, Lifeline, Message, Receiver, Resource, Sender, Service, Task,
 };
+
+#[cfg(feature = "dyn-bus")]
+pub use crate::lifeline_bus;
