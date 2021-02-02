44fb4439e84ecdc8dc8d2fedd97b1ca77f16fde1
Austin Jones <implAustin@gmail.com>
2021-02-01 23:04:42 -0500

    Update to postage v0.4

--- src/channel/postage.rs
+++ src/channel/postage.rs
@@ -4,9 +4,71 @@ use crate::{impl_channel_clone, impl_channel_take};
 use async_trait::async_trait;
 use postage::sink::Sink;
 use postage::stream::Stream;
-use postage::{barrier, broadcast, mpsc, oneshot, watch};
+use postage::{barrier, broadcast, dispatch, mpsc, oneshot, watch};
 use std::fmt::Debug;
 
+// barrier
+impl Channel for barrier::Sender {
+    type Tx = Self;
+    type Rx = barrier::Receiver;
+
+    fn channel(_capacity: usize) -> (Self::Tx, Self::Rx) {
+        barrier::channel()
+    }
+
+    fn default_capacity() -> usize {
+        1
+    }
+}
+
+impl_storage_take!(barrier::Sender);
+impl_storage_clone!(barrier::Receiver);
+
+// broadcast
+impl<T: Clone + Send + 'static> Channel for broadcast::Sender<T> {
+    type Tx = Self;
+    type Rx = broadcast::Receiver<T>;
+
+    fn channel(capacity: usize) -> (Self::Tx, Self::Rx) {
+        broadcast::channel(capacity)
+    }
+
+    fn default_capacity() -> usize {
+        16
+    }
+
+    fn clone_rx(rx: &mut Option<Self::Rx>, tx: Option<&Self::Tx>) -> Option<Self::Rx> {
+        rx.take().or_else(|| tx.map(|tx| tx.subscribe()))
+    }
+}
+
+impl_channel_clone!(broadcast::Sender<T>);
+impl_channel_take!(broadcast::Receiver<T>);
+
+#[async_trait]
+impl<T> crate::Sender<T> for broadcast::Sender<T>
+where
+    T: Clone + Debug + Send,
+{
+    async fn send(&mut self, value: T) -> Result<(), LifelineSendError<T>> {
+        Sink::send(self, value)
+            .await
+            .map(|_| ())
+            .map_err(|err| LifelineSendError::Return(err.0))
+    }
+}
+
+#[async_trait]
+impl<T> crate::Receiver<T> for broadcast::Receiver<T>
+where
+    T: Clone + Debug + Send,
+{
+    async fn recv(&mut self) -> Option<T> {
+        Stream::recv(self).await
+    }
+}
+
+// mpsc
 impl<T: Send + 'static> Channel for mpsc::Sender<T> {
     type Tx = Self;
     type Rx = mpsc::Receiver<T>;
@@ -45,50 +107,46 @@ where
     }
 }
 
-impl<T: Clone + Send + 'static> Channel for broadcast::Sender<T> {
+// dispatch
+impl<T: Send + 'static> Channel for dispatch::Sender<T> {
     type Tx = Self;
-    type Rx = broadcast::Receiver<T>;
+    type Rx = dispatch::Receiver<T>;
 
     fn channel(capacity: usize) -> (Self::Tx, Self::Rx) {
-        broadcast::channel(capacity)
+        dispatch::channel(capacity)
     }
 
     fn default_capacity() -> usize {
         16
     }
-
-    fn clone_rx(rx: &mut Option<Self::Rx>, tx: Option<&Self::Tx>) -> Option<Self::Rx> {
-        rx.take().or_else(|| tx.map(|tx| tx.subscribe()))
-    }
 }
 
-impl_channel_clone!(broadcast::Sender<T>);
-
-impl_channel_take!(broadcast::Receiver<T>);
+impl_channel_clone!(dispatch::Sender<T>);
+impl_channel_clone!(dispatch::Receiver<T>);
 
 #[async_trait]
-impl<T> crate::Sender<T> for broadcast::Sender<T>
+impl<T> crate::Sender<T> for dispatch::Sender<T>
 where
-    T: Clone + Debug + Send,
+    T: Debug + Send,
 {
     async fn send(&mut self, value: T) -> Result<(), LifelineSendError<T>> {
         Sink::send(self, value)
             .await
-            .map(|_| ())
             .map_err(|err| LifelineSendError::Return(err.0))
     }
 }
 
 #[async_trait]
-impl<T> crate::Receiver<T> for broadcast::Receiver<T>
+impl<T> crate::Receiver<T> for dispatch::Receiver<T>
 where
-    T: Clone + Debug + Send,
+    T: Debug + Send,
 {
     async fn recv(&mut self) -> Option<T> {
         Stream::recv(self).await
     }
 }
 
+// oneshot
 impl<T: Send + 'static> Channel for oneshot::Sender<T> {
     type Tx = Self;
     type Rx = oneshot::Receiver<T>;
@@ -145,19 +203,3 @@ where
         Stream::recv(self).await
     }
 }
-
-impl Channel for barrier::Sender {
-    type Tx = Self;
-    type Rx = barrier::Receiver;
-
-    fn channel(_capacity: usize) -> (Self::Tx, Self::Rx) {
-        barrier::channel()
-    }
-
-    fn default_capacity() -> usize {
-        1
-    }
-}
-
-impl_storage_take!(barrier::Sender);
-impl_storage_clone!(barrier::Receiver);
